a76787b2b9cc5ab134051a91a517363cd51a9ffe
Avishek Biswas <avishek.biswas@g.bracu.ac.bd>
2024-01-27 23:58:35 +0600

    sff

---
+++ Ex.java
@@ -0,0 +1,19 @@
+public class Ex {
+
+    // Write Hello World 10 times
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    
+    }
+    
+}
+
---
+++ Use.java
@@ -0,0 +1,7 @@
+public class Use { 
+
+    public static void main(String[] args) { 
+    
+        System.out.println("Hi " + args[0] + ". How are you?");
+    } 
+}
